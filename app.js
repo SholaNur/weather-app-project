627fb9a7132ac621fa4d8018448360ba96b45cb0
Shola Nur <shola.nur@gmail.com>
2023-04-04 11:36:48 -0700

    post function added

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 const https = require("https");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=d3c30d5bbdee3b395bab9de6f880b297&units=metric";
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const query = req.body.cityName;
+  const apiKey = "d3c30d5bbdee3b395bab9de6f880b297";
+  const unit = "metric";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${unit}`;
 
   https.get(url, (response) => {
     console.log(response.statusCode);
@@ -16,9 +25,9 @@ app.get("/", (req, res) => {
       const iconCode = weatherData.weather[0].icon;
       const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
       res.send(`
-        <h1>The weather in Seattle is ${temp} and ${weatherDescription} right now</h1>
-        <img src="${iconUrl}" alt="Weather icon">
-      `);
+          <h1>The weather in ${query} is ${temp} Celcius and ${weatherDescription} right now</h1>
+          <img src="${iconUrl}" alt="Weather icon">
+        `);
     });
   });
 });
